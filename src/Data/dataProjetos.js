7e61f9b0780521cfd892b31e60371420da6859a9
reinaldoper <reinaldoper83@gmail.com>
2023-02-23 08:34:39 -0400

    feat: adicionado link

--- src/Data/dataProjetos.js
+++ src/Data/dataProjetos.js
@@ -5,42 +5,63 @@ import login from '../images/tela-login.png';
 import partidas from '../images/partidas.png';
 import skambooks from '../images/skambooks.png';
 import skambooks1 from '../images/skambooks1.png';
+import receitas from '../images/receitas.png';
+import tela from '../images/tela-classificacao.png';
 
 const data = [
   {
     id: 1,
     title: 'Mercado Livre',
     image: mercado,
+    link: 'https://github.com/reinaldoper/mercadoLivre'
   },  
   {
     id: 2,
     title: 'Musicas- Trybe Tunes',
     image: musicas,
+    link: 'https://github.com/reinaldoper?tab=repositories'
   },
   {
     id: 3,
     title: 'Times- Ligas mundiais',
     image: times,
+    link: 'https://github.com/reinaldoper/ligasDeTimes'
   },
   {
     id: 4,
     title: 'Login- Projeto Trybe futebol',
     image: login,
+    link: 'https://github.com/reinaldoper/trybe-futebol-clube'
   },
   {
     id: 5,
     title: 'Partidas- Projeto Trybe futebol',
     image: partidas,
+    link: 'https://github.com/reinaldoper/trybe-futebol-clube'
   },
   {
     id: 6,
     title: 'Skambooks- Projeto de pesquisas por livros',
     image: skambooks,
+    link: 'https://github.com/stonefullstm/project-skambooks'
   },
   {
     id: 7,
     title: 'Skambooks- Projeto de pesquisas por livros',
     image: skambooks1,
+    link: 'https://github.com/stonefullstm/project-skambooks'
+  },
+  {
+    id: 8,
+    title: 'Receitas- Projeto Receitas',
+    image: receitas,
+    link: 'https://github.com/reinaldoper?tab=repositories'
+  },
+  {
+    id: 9,
+    title: 'Tela- Classificacao',
+    image: tela,
+    link: 'https://github.com/reinaldoper/trybe-futebol-clube'
   }
 ]
 
--- src/components/projetos.jsx
+++ src/components/projetos.jsx
@@ -6,8 +6,8 @@ export default class projetos extends Component {
   render() {
     const result = data.map((data) => (
       <div key={data.id}>
-        <h2>{data.title}</h2>
-        <img src={data.image} alt={data.title} className='map-img'/>
+        <h2 className='title'>{data.title}</h2>
+        <a href={data.link}><img src={data.image} alt={data.title} className='map-img'/></a>
       </div>
     ));
     return (
